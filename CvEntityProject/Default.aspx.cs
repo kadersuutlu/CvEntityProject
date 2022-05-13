abb7acc0466c7698d8dc52878fcbc945bd53afcc
Kader Sutlu <80063396+kadersuutlu@users.noreply.github.com>
2022-05-13 11:00:13 +0300

    theme change

--- CvEntityProject/Default.aspx.cs
+++ CvEntityProject/Default.aspx.cs
@@ -9,8 +9,17 @@ namespace CvEntityProject
 {
     public partial class Default : System.Web.UI.Page
     {
+        DBCVENTITYEntities db=new DBCVENTITYEntities();
         protected void Page_Load(object sender, EventArgs e)
         {
+            Repeater1.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater1.DataBind();
+            Repeater2.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater2.DataBind();
+            Repeater3.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater3.DataBind();
+            Repeater4.DataSource = db.TBLYETENEKLER.ToList();
+            Repeater4.DataBind();
 
         }
     }
